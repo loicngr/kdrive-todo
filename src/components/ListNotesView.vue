3b5bdf3953f31511e83bc7ff3e1ef541fefd94fc
lnogier <loicnogier@ik.me>
2024-03-09 13:12:59 +0000

    :sparkles: feature/#33 - Editeur - Améliorations (#37)
    - Ajout d'éléments dans la toolbar
     - link
     - font size / family
     - source code
     - full screen

--- src/components/ListNotesView.vue
+++ src/components/ListNotesView.vue
@@ -99,6 +99,16 @@
                 height="80vh"
                 max-height="1200px"
                 :toolbar="editor.toolbar"
+                :fonts="{
+                  arial: 'Arial',
+                  arial_black: 'Arial Black',
+                  comic_sans: 'Comic Sans MS',
+                  courier_new: 'Courier New',
+                  impact: 'Impact',
+                  lucida_grande: 'Lucida Grande',
+                  times_new_roman: 'Times New Roman',
+                  verdana: 'Verdana'
+                }"
                 content-class=""
                 @keyup.enter.stop
               />
@@ -332,7 +342,60 @@ const editor = reactive({
   toolbar: [
     ['left', 'center', 'right', 'justify'],
     ['bold', 'italic', 'underline', 'strike'],
+    ['hr', 'link', 'fullscreen'],
+    [
+      {
+        label: $q.lang.editor.formatting,
+        icon: $q.iconSet.editor.formatting,
+        list: 'no-icons',
+        options: [
+          'p',
+          'h1',
+          'h2',
+          'h3',
+          'h4',
+          'h5',
+          'h6',
+          'code',
+        ],
+      },
+      {
+        label: $q.lang.editor.fontSize,
+        icon: $q.iconSet.editor.fontSize,
+        fixedLabel: true,
+        fixedIcon: true,
+        list: 'no-icons',
+        options: [
+          'size-1',
+          'size-2',
+          'size-3',
+          'size-4',
+          'size-5',
+          'size-6',
+          'size-7',
+        ],
+      },
+      {
+        label: $q.lang.editor.defaultFont,
+        icon: $q.iconSet.editor.font,
+        fixedIcon: true,
+        list: 'no-icons',
+        options: [
+          'default_font',
+          'arial',
+          'arial_black',
+          'comic_sans',
+          'courier_new',
+          'impact',
+          'lucida_grande',
+          'times_new_roman',
+          'verdana',
+        ],
+      },
+      'removeFormat',
+    ],
     ['undo', 'redo'],
+    ['viewsource'],
   ],
 })
 
